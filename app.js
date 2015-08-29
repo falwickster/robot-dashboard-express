6a15268d836df1a9080eb901cd29d614104db944
Krzysztof Jafra <falwickster@gmail.com>
2015-08-29 12:27:49 +0200

    Narazie wszystko działa, biblioteki z licznikami w katalogu, ale nie używane

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ client.on('connect', function () {
 
 client.on('message', function (topic, message) {
   // message is Buffer
-  //console.log(message.toString());
+  console.log(message.toString());
   //client.end();
     io.emit('mqtt', message.toString());
 });
